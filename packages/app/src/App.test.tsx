6ff090eec0c530af844484ebbb5fc33b2d2a76e1
KHeo <sainthkh@naver.com>
2021-02-23 10:08:38 +0900

    fix: <App> unit test.

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test( 'renders learn react link', () => {
+test( 'renders start quiz link', () => {
 	render( <App /> );
-	const linkElement = screen.getByText( /learn react/i );
+	const linkElement = screen.getByText( /Start Quiz/i );
 	expect( linkElement ).toBeInTheDocument();
 } );
